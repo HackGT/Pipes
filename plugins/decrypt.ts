cbf7a218b9b686c86843645a64d093e1eb9add67
Ryan Petschek <petschekr@gmail.com>
2017-06-25 08:41:04 -0700

    Add NLP support for decryption module

--- plugins/decrypt.ts
+++ plugins/decrypt.ts
@@ -1,8 +1,8 @@
 import * as crypto from "crypto";
 
 export let name = "Decryption";
+export let verbs = ["decrypt"];
 export let inputs = {
-    "authTag": "string",
     "encrypted": "string"
 }
 export let outputs = {
@@ -16,8 +16,9 @@ export let requires = {
 export let run = (requires: any, input: any) => {
     return new Promise<any>((resolve, reject) => {
         let decipher = crypto.createDecipher("id-aes256-GCM", requires.password);
-        decipher.setAuthTag(Buffer.from(input.authTag as string, "hex"));
-        let decrypted = decipher.update(input.encrypted, "hex", "utf8");
+        let {authTag, encrypted} = JSON.parse(input.encrypted);
+        decipher.setAuthTag(Buffer.from(authTag as string, "hex"));
+        let decrypted = decipher.update(encrypted, "hex", "utf8");
         try {
             decrypted += decipher.final("utf8");
         }
@@ -29,4 +30,20 @@ export let run = (requires: any, input: any) => {
             "data": decrypted
         });
     });
+}
+
+export function parse_language(verb: string, tokens: any[]) {
+    let text: string[] = [];
+
+    for (let token of tokens) {
+        text.push(token.text.content);
+    }
+
+    if (text.length === 1 && text[0] === "it") {
+        text.pop();
+    }
+
+    return {
+        encrypted: text.length === 0 ? null : text.join(" ")
+    }
 }
