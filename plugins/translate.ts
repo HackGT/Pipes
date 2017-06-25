ab7528d36f7f6d08b17554c42737c9af9024bd54
Ehsan Asdar <ehsanmasdar@gmail.com>
2017-06-24 17:42:15 -0700

    Add translate plugin

---
+++ plugins/translate.ts
@@ -0,0 +1,36 @@
+const Translate = require('@google-cloud/translate');
+const projectId = "testfirstapp1";
+
+// TODO
+// import * as Twitter from "twitter";
+
+export let inputs = {
+    "text": "text",
+    "langauge": "text"
+}
+
+export let outputs = {
+    "out": "text"
+}
+
+export let requires = {
+    api_key: "text"
+};
+
+export let run = (requires: any, input: any) => {
+    return new Promise<string>((resolve, reject) => {
+        const translateClient = Translate({
+            projectId: projectId,
+            key: requires.api_key
+        });
+        translateClient.translate(input.text, input.language)
+        .then((results: any) => {
+            const translation = results[0];
+            resolve(translation);
+        })
+        .catch((err: any) => {
+            console.error('ERROR:', err);
+            reject(err);
+        });
+    });        
+};
